eceaa82dc964bc3ab1a7cb4235d0887e056e62eb
Bruno Mijail Díaz Barba <bruno.diaz@softtekprojects.com>
2024-01-25 00:37:34 -0600

    Force remove Project_calc submodule from staging

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function handleMath(symbol){
 function flushOperation(intBuffer){
     if(previousOperator === '+'){
         runningTotal += intBuffer;
-    }else if(previousOperator === '-'){
+    }else if(previousOperator === '−'){
         runningTotal -= intBuffer;
     }else if(previousOperator === '×'){
         runningTotal *= intBuffer;
---
+++ test.py
@@ -0,0 +1,5 @@
+a, b = 0, 1
+while b <=2584:
+    print(b, end=" ")
+    a, b = b, a + b
+    
